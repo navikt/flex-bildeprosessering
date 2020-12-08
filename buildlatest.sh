27f37b2abd6608791917ccd37729958ec52d9f47
kulsveen <bjorn.inge.kulsveen@nav.no>
2020-12-08 10:42:15 +0100

    Bruker samme versjon av sharp

--- buildlatest.sh
+++ buildlatest.sh
@@ -2,7 +2,7 @@ echo "Bygger flex-bildeprosessering latest for docker compose utvikling"
 
 npm i
 rm -rf node_modules/sharp
-npm install --arch=x64 --platform=linux --target=8.10.0 sharp   # For å kjøre i docker linux-x64
+npm install --arch=x64 --platform=linux --target=8.10.0 sharp@0.26.3   # For å kjøre i docker linux-x64
 npm run build
 
 docker build . -t flex-bildeprosessering:latest
