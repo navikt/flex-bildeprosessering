c127f449a1afcbecc89c0144308a1e35a2dc87f4
Kent Daleng <kent.daleng@nav.no>
2020-12-15 13:50:30 +0100

    logger strukturert, inkluderer content-type, osv.

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,13 @@ import express from "express";
 import cors from 'cors';
 import sharp from 'sharp';
 import multer from 'multer';
+import winston from 'winston';
+
+const logger = winston.createLogger({
+    transports: [
+      new winston.transports.Console(),
+    ]
+});
 
 const app = express();
 const port = 8080; // default port to listen
@@ -31,27 +38,31 @@ app.post("/prosesser",
     upload.single('file'),
     corsHandler,
     async (req, res) => {
-        console.log('Mottok data: ' + req.file.size + ' bytes ' + req.file.mimetype)
-
-        sharp(req.file.buffer)
-            .resize(600, 1200, {
-                fit: sharp.fit.inside,          // Beholder ratio, men går ikke over høyde eller bredde
-            })
-            .toFormat('jpg', {
-                quality: 100
-            })
-            .toBuffer()
-            .then((img) => {
-                res.send(img)
-                console.log('Sendte tilbake: ' + img.byteLength + ' bytes jpg')
-            })
-            .catch((reason) => {
-                console.log('Feil: ' + reason.message)
-                res.status(500)
-            })
+        if (req.file === undefined) {
+            logger.log('error', 'mottok ikke data')
+            res.sendStatus(500)
+            return
+        }
+        logger.log('info', 'Mottok data: ' + req.file.size + ' bytes ' + req.file.mimetype)
+        try {
+            const img = await sharp(req.file.buffer)
+                .resize(600, 1200, {
+                    fit: sharp.fit.inside,          // Beholder ratio, men går ikke over høyde eller bredde
+                })
+                .toFormat('jpg', {
+                    quality: 100
+                })
+                .toBuffer()
+            res.set("Content-Type", "image/jpg")
+            res.send(img)
+            logger.log('info', 'Sendte tilbake: ' + img.byteLength + ' bytes jpg')
+        } catch (reason) {
+            logger.log('error', reason.message)
+            res.sendStatus(500)
+        }
     })
 
 // start the Express server
 app.listen(port, () => {
-    console.log(`server started at port ${port}`);
+    logger.log('info', `server started at port ${port}`);
 })
